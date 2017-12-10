7e6dcaecd6ac5e3dde67700aa5004b57b18aad64
Tina Vivio <tina@sparkfund.co>
2017-12-09 16:07:58 -0800

    update spark code

--- src/main/java/spark/SkiierSparkApp.java
+++ src/main/java/spark/SkiierSparkApp.java
@@ -1,13 +1,5 @@
 package spark;
 
-import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaRDD;
-import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.api.java.function.Function;
-import org.apache.spark.api.java.function.Function2;
-
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Properties;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
@@ -16,10 +8,12 @@ import org.apache.spark.sql.SparkSession;
 public final class SkiierSparkApp {
 
     public static void main(String[] args) throws Exception {
+        
         String url = "jdbc:postgresql://skierdbinstance.cmt5itoksgaz.us-west-2.rds.amazonaws.com/skierdb";
         SparkSession spark = SparkSession.builder()
                 .appName("Simple Application")
                 .getOrCreate();
+        
         Dataset<Row> rides = spark
                 .read()
                 .format("jdbc")
@@ -29,6 +23,7 @@ public final class SkiierSparkApp {
                 .option("user", "tinavivio")
                 .option("password", "rahul2016")
                 .load();
+        
         Dataset<Row> lifts = spark
                 .read()
                 .format("jdbc")
@@ -39,17 +34,54 @@ public final class SkiierSparkApp {
                 .option("password", "rahul2016")
                 .load();
         
-        Dataset<Row> countsByLiftNumberAndDayNumber = rides.groupBy("liftNumber", "dayNumber").count();
-        Dataset<Row> mostPopularLiftByDay = countsByLiftNumberAndDayNumber.groupBy("dayNumber").agg(org.apache.spark.sql.functions.max("count"));
-        Dataset<Row> countsBySkierIdAndDayNumberAndLiftNumber = rides.groupBy("skierId", "dayNumber", "liftNumber").count();
+        Dataset<Row> countsByLiftNumberAndDayNumber = rides
+                .groupBy("liftNumber", "dayNumber")
+                .count();
+        Dataset<Row> maxCountByDay = countsByLiftNumberAndDayNumber
+                .groupBy("dayNumber")
+                .agg(org.apache.spark.sql.functions.max("count"));
+        Dataset<Row> mostPopularLiftsByDay = countsByLiftNumberAndDayNumber
+                .join(maxCountByDay, "dayNumber")
+                .where(maxCountByDay.col("max(count)")
+                        .equalTo(countsByLiftNumberAndDayNumber.col("count")))
+                .select(countsByLiftNumberAndDayNumber.col("dayNumber"), 
+                        countsByLiftNumberAndDayNumber.col("liftNumber"), 
+                        countsByLiftNumberAndDayNumber.col("count"));
+        Dataset<Row> countsBySkierIdAndDayNumberAndLiftNumber = rides
+                .groupBy("skierId", "dayNumber", "liftNumber")
+                .count();
         Dataset<Row> totalHeightBySkierIdAndDayNumberAndLiftNumber = countsBySkierIdAndDayNumberAndLiftNumber
                 .join(lifts)
-                .where(countsBySkierIdAndDayNumberAndLiftNumber.col("liftNumber").equalTo(lifts.col("liftNumber")))
+                .where(countsBySkierIdAndDayNumberAndLiftNumber.col("liftNumber")
+                        .equalTo(lifts.col("liftNumber")))
                 .select(countsBySkierIdAndDayNumberAndLiftNumber.col("skierId"), 
                         countsBySkierIdAndDayNumberAndLiftNumber.col("dayNumber"), 
-                        countsBySkierIdAndDayNumberAndLiftNumber.col("count").multiply(lifts.col("height")).alias("totalHeightForDayAndLift"));
-        Dataset<Row> totalHeightBySkierIdAndDayNumber = totalHeightBySkierIdAndDayNumberAndLiftNumber.groupBy("skierId", "dayNumber").agg(org.apache.spark.sql.functions.sum("totalHeightForDayAndLift").alias("totalHeightForDay"));
-        Dataset<Row> mostProlificSkierByDay = totalHeightBySkierIdAndDayNumber.groupBy("dayNumber").agg(org.apache.spark.sql.functions.max("totalHeightForDay"));
+                        countsBySkierIdAndDayNumberAndLiftNumber.col("count")
+                                .multiply(lifts.col("height"))
+                                .alias("totalHeightForDayAndLift"));
+        Dataset<Row> totalHeightBySkierIdAndDayNumber = totalHeightBySkierIdAndDayNumberAndLiftNumber
+                .groupBy("skierId", "dayNumber")
+                .agg(org.apache.spark.sql.functions.sum("totalHeightForDayAndLift")
+                        .alias("totalHeightForDay"));
+        Dataset<Row> maxHeightByDay = totalHeightBySkierIdAndDayNumber
+                .groupBy("dayNumber")
+                .agg(org.apache.spark.sql.functions.max("totalHeightForDay"));
+        Dataset<Row> mostProlificSkiersByDay = totalHeightBySkierIdAndDayNumber
+                .join(maxHeightByDay, "dayNumber")
+                .where(maxHeightByDay.col("max(totalHeightForDay)")
+                        .equalTo(totalHeightBySkierIdAndDayNumber.col("totalHeightForDay")))
+                .select(totalHeightBySkierIdAndDayNumber.col("dayNumber"), 
+                        totalHeightBySkierIdAndDayNumber.col("skierId"), 
+                        totalHeightBySkierIdAndDayNumber.col("totalHeightForDay"));
+        
+        /*countsByLiftNumberAndDayNumber.coalesce(1).write().format("json").save("./results");
+        maxCountByDay.coalesce(1).write().format("json").save("./results-2");
+        mostPopularLiftsByDay.coalesce(1).write().format("json").save("./results-3");
+        countsBySkierIdAndDayNumberAndLiftNumber.coalesce(1).write().format("json").save("./results-4");
+        totalHeightBySkierIdAndDayNumberAndLiftNumber.coalesce(1).write().format("json").save("./results-5");
+        totalHeightBySkierIdAndDayNumber.coalesce(1).write().format("json").save("./results-6");
+        maxHeightByDay.coalesce(1).write().format("json").save("./results-7");
+        mostProlificSkiersByDay.coalesce(1).write().format("json").save("./results-8");*/
         
         Properties connectionProperties = new Properties();
         connectionProperties.put("user", "tinavivio");
@@ -57,11 +89,16 @@ public final class SkiierSparkApp {
         connectionProperties.put("driver", "org.postgresql.Driver");
         
         countsByLiftNumberAndDayNumber.coalesce(1).write().jdbc(url, "countsByLiftNumberAndDayNumber", connectionProperties);
-        mostPopularLiftByDay.coalesce(1).write().jdbc(url, "mostPopularLiftByDay", connectionProperties);
+        maxCountByDay.coalesce(1).write().jdbc(url, "maxCountByDay", connectionProperties);
+        mostPopularLiftsByDay.coalesce(1).write().jdbc(url, "mostPopularLiftsByDay", connectionProperties);
         countsBySkierIdAndDayNumberAndLiftNumber.coalesce(1).write().jdbc(url, "countsBySkierIdAndDayNumberAndLiftNumber", connectionProperties);
+        totalHeightBySkierIdAndDayNumberAndLiftNumber.coalesce(1).write().jdbc(url, "totalHeightBySkierIdAndDayNumberAndLiftNumber", connectionProperties);
         totalHeightBySkierIdAndDayNumber.coalesce(1).write().jdbc(url, "totalHeightBySkierIdAndDayNumber", connectionProperties);
-        mostProlificSkierByDay.coalesce(1).write().jdbc(url, "mostProlificSkierByDay", connectionProperties);
+        maxHeightByDay.coalesce(1).write().jdbc(url, "maxHeightByDay", connectionProperties);
+        mostProlificSkiersByDay.coalesce(1).write().jdbc(url, "mostProlificSkiersByDay", connectionProperties);
         
         spark.stop();
+        
     }
+    
 }
